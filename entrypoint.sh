bb7f4a11699ec6aee6a2117a95e63986e46115c8
hongchenoutlook <105474428+hongchenoutlook@users.noreply.github.com>
2023-02-21 10:52:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,7 +36,7 @@ Argo_xray_vmess="vmess://$(echo -n "\
 \"net\": \"ws\",\
 \"type\": \"none\",\
 \"host\": \"${ARGO}\",\
-\"path\": \"/$uuid-vm\",\
+\"path\": \"/$uuid-ring\",\
 \"tls\": \"tls\",\
 \"sni\": \"${ARGO}\"\
 }"\
